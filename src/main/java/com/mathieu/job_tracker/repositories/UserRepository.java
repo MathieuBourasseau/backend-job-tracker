cba1c74b8b7df61318c537dbd8fc7ba45439d5c6
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-01 16:14:47 +0200

    feat: method findByEmail added to UserRepository in order to check if an email adress is already existing in the db.

--- src/main/java/com/mathieu/job_tracker/repositories/UserRepository.java
+++ src/main/java/com/mathieu/job_tracker/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.mathieu.job_tracker.repositories;
 import com.mathieu.job_tracker.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 // Empty interface: Spring Data JPA generates the implementation (save, findAll, findById, deleteById...)
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
+    
+    Optional<User> findByEmail(String email);
 }
