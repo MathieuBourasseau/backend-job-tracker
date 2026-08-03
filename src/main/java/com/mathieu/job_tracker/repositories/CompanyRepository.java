8addc68bf28d0392676b000964529a11b484bccb
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-03 11:08:58 +0200

    feat: findByName method added.

--- src/main/java/com/mathieu/job_tracker/repositories/CompanyRepository.java
+++ src/main/java/com/mathieu/job_tracker/repositories/CompanyRepository.java
@@ -1,11 +1,17 @@
 package com.mathieu.job_tracker.repositories;
 
 import com.mathieu.job_tracker.models.Company;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 // Empty interface: Spring Data JPA generates the implementation (save, findAll, findById, deleteById...)
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long>{
+
+    // Method to search a company by its name
+    Optional<Company> findByName(String name); 
 
 }
