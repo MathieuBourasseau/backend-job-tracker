79f00e36fc8578d53d511ccd9538209e00388e10
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-03 11:05:32 +0200

    feat: method findByUserId added.

--- src/main/java/com/mathieu/job_tracker/repositories/ApplicationRepository.java
+++ src/main/java/com/mathieu/job_tracker/repositories/ApplicationRepository.java
@@ -1,6 +1,9 @@
 package com.mathieu.job_tracker.repositories;
 
 import com.mathieu.job_tracker.models.Application;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long>{
 
+    // Method added in addition to the CRUD methods of JPA
+    List<Application> findByUserId(Long userId);
+    
 }
