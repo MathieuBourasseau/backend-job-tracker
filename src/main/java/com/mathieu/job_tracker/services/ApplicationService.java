09a640bf21ec1c3adee3388e4832598ea4bc0693
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-03 11:18:42 +0200

    feat: ApplicationService.java created and imports done in order to create method createApplication()

---
+++ src/main/java/com/mathieu/job_tracker/services/ApplicationService.java
@@ -0,0 +1,12 @@
+package com.mathieu.job_tracker.services;
+
+import com.mathieu.job_tracker.dto.ApplicationCreateDto;
+import com.mathieu.job_tracker.dto.ApplicationResponseDto;
+import com.mathieu.job_tracker.models.Application;
+import com.mathieu.job_tracker.repositories.ApplicationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationService {
+    
+}
