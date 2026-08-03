a29b2c0d90b3054d94ec188aa9f124025f1dfa1c
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-03 17:05:00 +0200

    feat: findByApplicationId method added in StatusRepository.

--- src/main/java/com/mathieu/job_tracker/repositories/StatusRepository.java
+++ src/main/java/com/mathieu/job_tracker/repositories/StatusRepository.java
@@ -1,11 +1,15 @@
 package com.mathieu.job_tracker.repositories;
 
 import com.mathieu.job_tracker.models.Status;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 // Empty interface: Spring Data JPA generates the implementation (save, findAll, findById, deleteById...)
 @Repository
 public interface StatusRepository extends JpaRepository<Status, Long>{
-
+    // Method to get an application by its id
+    List<Status>findByApplicationId(Long id);
 }
