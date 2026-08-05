82ccd56e831e3a2b74f7aa4a8fc9f898b007bc8f
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-05 18:28:44 +0200

    feat: UserController created with imports needed for this controller.

---
+++ src/main/java/com/mathieu/job_tracker/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.mathieu.job_tracker.controllers;
+
+import com.mathieu.job_tracker.dto.UserCreateDto;
+import com.mathieu.job_tracker.models.User;
+import com.mathieu.job_tracker.services.UserService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class UserController {
+    
+}
