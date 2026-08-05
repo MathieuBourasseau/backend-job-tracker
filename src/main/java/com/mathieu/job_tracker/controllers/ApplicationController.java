4971cf47679429d1f4d72e7306845c020334f54e
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-05 18:49:02 +0200

    Feat: beginning of ApplicationController created with imports, service and constructor required.

---
+++ src/main/java/com/mathieu/job_tracker/controllers/ApplicationController.java
@@ -0,0 +1,21 @@
+package com.mathieu.job_tracker.controllers;
+
+import com.mathieu.job_tracker.dto.ApplicationCreateDto;
+import com.mathieu.job_tracker.dto.ApplicationResponseDto;
+import com.mathieu.job_tracker.services.ApplicationService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/applications")
+public class ApplicationController {
+    
+    // Service required
+    private final ApplicationService applicationService;
+
+    public ApplicationController(ApplicationService applicationService){
+        this.applicationService = applicationService;
+    }
+}
