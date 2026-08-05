41bb99286bcd95a0b7cffd2a988499f8215199d6
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-05 19:12:55 +0200

    Feat: GET method added in ApplicationController.java to get all applications of a user.

--- src/main/java/com/mathieu/job_tracker/controllers/ApplicationController.java
+++ src/main/java/com/mathieu/job_tracker/controllers/ApplicationController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/applications")
 public class ApplicationController {
@@ -24,5 +26,12 @@ public class ApplicationController {
     public ResponseEntity<ApplicationResponseDto> createApplication(@PathVariable Long userId, @RequestBody ApplicationCreateDto dto){
         ApplicationResponseDto createdApplication = applicationService.createApplication(dto, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdApplication);
+    }
+
+    // Route to get all user's application
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<ApplicationResponseDto>> getUserApplications(@PathVariable Long userId){
+        List<ApplicationResponseDto> userApplications = applicationService.getUserApplications(userId);
+        return ResponseEntity.ok(userApplications);
     }
 }
