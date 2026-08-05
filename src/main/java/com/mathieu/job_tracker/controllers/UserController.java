2b1ad3d5675eb5fedf52eca8c6bfc58dfa19ac74
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-05 18:41:15 +0200

    Fix: removed User model import and added / before api/users

--- src/main/java/com/mathieu/job_tracker/controllers/UserController.java
+++ src/main/java/com/mathieu/job_tracker/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.mathieu.job_tracker.controllers;
 
 import com.mathieu.job_tracker.dto.UserCreateDto;
 import com.mathieu.job_tracker.dto.UserResponseDto;
-import com.mathieu.job_tracker.models.User;
 import com.mathieu.job_tracker.services.UserService;
 
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 public class UserController {
     
     // Services required for the use of the controller
