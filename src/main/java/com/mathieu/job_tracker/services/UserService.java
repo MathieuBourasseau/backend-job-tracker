cd679971cc4c23e9c3ac93d216e029b1d6c4eaf8
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-06 17:31:08 +0200

    feat: password now hashed with PasswordEncoder before saving a new user.

--- src/main/java/com/mathieu/job_tracker/services/UserService.java
+++ src/main/java/com/mathieu/job_tracker/services/UserService.java
@@ -5,6 +5,8 @@ import com.mathieu.job_tracker.dto.UserResponseDto;
 import com.mathieu.job_tracker.exceptions.EmailAlreadyExistsException;
 import com.mathieu.job_tracker.models.User;
 import com.mathieu.job_tracker.repositories.UserRepository;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +15,14 @@ public class UserService {
     // Repository required to get access to its methods
     private final UserRepository userRepository;
 
+    // Required to hash password
+    private final PasswordEncoder passwordEncoder;
+
+
     // UserServices needs the repository to use the business logic
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // Methods of this service
@@ -29,8 +36,8 @@ public class UserService {
         }
 
         // If conditions are valid we can create and save the new user in DB
-
-        User newUser = new User(dto.getEmail(), dto.getPassword());
+        // Password is hashed before saving it in DB
+        User newUser = new User(dto.getEmail(), passwordEncoder.encode(dto.getPassword()));
         User savedUser = userRepository.save(newUser);
 
         UserResponseDto response = new UserResponseDto(savedUser.getEmail(), savedUser.getId());
