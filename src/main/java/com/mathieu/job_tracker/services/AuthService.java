68e08b9f4aef764cf6ccf1604ddbf85bfe36066c
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-07 16:44:26 +0200

    feat: email and password caught from LoginCreateDto. Then a security check is done to make sure that email and password are valid. We display on purpose the same message in case of exception to avoid a potential hacker to get precise information.

--- src/main/java/com/mathieu/job_tracker/services/AuthService.java
+++ src/main/java/com/mathieu/job_tracker/services/AuthService.java
@@ -1,5 +1,9 @@
 package com.mathieu.job_tracker.services;
 
+import com.mathieu.job_tracker.dto.LoginCreateDto;
+import com.mathieu.job_tracker.dto.LoginResponseDto;
+import com.mathieu.job_tracker.exceptions.InvalidCredentialsException;
+import com.mathieu.job_tracker.models.User;
 import com.mathieu.job_tracker.repositories.UserRepository;
 import com.mathieu.job_tracker.security.JwtUtil;
 
@@ -20,5 +24,31 @@ public class AuthService {
         this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    // --- LOGIN METHOD --- 
+    public LoginResponseDto login(LoginCreateDto dto) {
+
+        // Get user data connection
+        String email = dto.getEmail();
+        String password = dto.getPassword();
+
+        // Checking user data connection validity
+        // If email is wrong
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new InvalidCredentialsException("Email ou mot de passe incorrect"));
+
+        // If password is wrong
+        if(!passwordEncoder.matches(password, user.getPassword())){
+            throw new InvalidCredentialsException("Email ou mot de passe inccorect");
+        }
+
+        
+
+
+
+
+        // Vérifier s'ils correspondent en base de données si l'un ou l'autre est faux on crée une new InvalidCredentialsException.java avec le message approprié
+        // Si ca correspond on créé le token avec le secret pet la signature
+        // Renvoyer le token vers le front 
     }
 }
