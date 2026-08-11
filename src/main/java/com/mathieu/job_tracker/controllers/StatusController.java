8d0cdb8649cbcc4b45e49342d5c7b14ae7c8a81c
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-08-11 18:23:57 +0200

    feat: authentication object and userId added in StatusController.

--- src/main/java/com/mathieu/job_tracker/controllers/StatusController.java
+++ src/main/java/com/mathieu/job_tracker/controllers/StatusController.java
@@ -6,6 +6,7 @@ import com.mathieu.job_tracker.services.StatusService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
@@ -24,8 +25,9 @@ public class StatusController {
 
     // POST route to create a new status
     @PostMapping
-    public ResponseEntity<ApplicationResponseDto> createStatus(@Valid @RequestBody StatusCreateDto dto){
-        ApplicationResponseDto createdStatus = statusService.createStatus(dto);
+    public ResponseEntity<ApplicationResponseDto> createStatus(Authentication authentication, @Valid @RequestBody StatusCreateDto dto){
+        Long userId = (Long) authentication.getPrincipal();
+        ApplicationResponseDto createdStatus = statusService.createStatus(userId,dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdStatus);
     }
 }
